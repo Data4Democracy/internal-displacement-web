09cd5a1e24471fc2961e0fac481d76562a3002cf
wwymak <wwymak@gmail.com>
2017-02-26 21:35:43 +0000

    add bootstrap for default css

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const Home = () => {
-  return <h2>Home...nothing here yet</h2>
+  return <h2>Home...nothing here yet<button className='btn btn-primary'>test</button></h2>
+
 }
 
 export default Home
